20f0654217c0c4e6403a4c7768bbe5c114d91319
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2024-06-09 16:30:40 +0200

    initial commit

---
+++ src/main/java/at/huemer/firstfabricmod/FirstFabricModClient.java
@@ -0,0 +1,11 @@
+package at.huemer.firstfabricmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstFabricModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
